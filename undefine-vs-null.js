c08ce54382e0a7904dfae143f547a3d0b4995c6d
exitdump <sheikhforid1026@gmail.com>
2020-08-09 13:26:58 +0600

    Null vs undefine for interview

---
+++ undefine-vs-null.js
@@ -0,0 +1,22 @@
+let name;
+console.log(name)
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+const result  = add(12, 12);
+console.log(result);
+
+function sub(num1, num2){
+    console.log(num1, num2);
+}
+const result2 = sub(12);
+console.log(result2);
+
+// const sum =(num1, num2) =>  num1 + num2;
+// const result3 = sum(34, 34);
+// console.log(result3);
+
+
+let fun = undefined;
+console.log(fun);
