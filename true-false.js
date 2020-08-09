b2764449ebe32c2c82dc01bfb29085837e9c3623
exitdump <sheikhforid1026@gmail.com>
2020-08-09 12:46:25 +0600

    added true-false

---
+++ true-false.js
@@ -0,0 +1,31 @@
+ //False condition
+ //0 is false
+ //null is false
+ //empty string is false("")
+ //undefine is false
+ //Nan is false
+ //false is false  
+//true condition
+//'0' is true
+// ' ' is true
+//[] is true
+
+
+ 
+ const age = 0;
+ if(age){
+     console.log('Condition is true');
+ }
+ else{
+     console.log('Condition is false');
+ }
+
+ //string condition check
+ // const name = {};
+  let name = 12; 
+if(name || name == 0){
+       console.log('Condition is true');
+   }
+   else{
+       console.log('Condition is false');
+   }
